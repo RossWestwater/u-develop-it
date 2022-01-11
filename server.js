b4b00566c217968a02a07489833d9fc031be08bb
Ross Westwater <ross.westwater@gmail.com>
2022-01-11 14:11:36 -0700

    connected server to database

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+const db = mysql.createConnection(
+  {
+  host: 'localhost',
+  user: 'root',
+  password: '6z8K#4O^w^9w',
+  database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
 
 app.get('/', (req, res) => {
   res.json({
     message: 'Hello World'
   });
 });
+
+db.query('SELECT * FROM candidates', (err, rows) => {
+  console.log(rows);
+});
+
 app.use((req, res) => {
   res.status(404).end();
 });
