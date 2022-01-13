65ac232d9792934d0ec13afd62c22343bd004cdc
Ross Westwater <ross.westwater@gmail.com>
2022-01-12 17:02:14 -0700

    added votes table and count tabulation

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-// Add near the top of the file
-const apiRoutes = require('./routes/apiRoutes');
-const db = require('./db/connection');
 const express = require('express');
+const db = require('./db/connection');
+const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -10,10 +9,10 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Add after Express middleware
+// Use apiRoutes
 app.use('/api', apiRoutes);
 
-// Not Found response for unmatched routes
+// Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
